7dc57dd83cd5b78627f3922bb92f03ced95c24cd
jansona <alelipona121@gmail.com>
2018-01-26 14:07:07 +0800

    first commit

---
+++ Myway-Lab1.1/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: W:/TI/ccs7.3/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: W:/TI/ccs7.3/ccsv7/ccs_base/msp430/include/msp430g2553.h
+main.obj: W:/TI/ccs7.3/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: W:/TI/ccs7.3/ccsv7/tools/compiler/ti-cgt-msp430_16.9.4.LTS/include/intrinsics.h
+main.obj: W:/TI/ccs7.3/ccsv7/tools/compiler/ti-cgt-msp430_16.9.4.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+W:/TI/ccs7.3/ccsv7/ccs_base/msp430/include/msp430.h: 
+W:/TI/ccs7.3/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+W:/TI/ccs7.3/ccsv7/ccs_base/msp430/include/in430.h: 
+W:/TI/ccs7.3/ccsv7/tools/compiler/ti-cgt-msp430_16.9.4.LTS/include/intrinsics.h: 
+W:/TI/ccs7.3/ccsv7/tools/compiler/ti-cgt-msp430_16.9.4.LTS/include/intrinsics_legacy_undefs.h: 
